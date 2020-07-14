3d9b9240f622e2c3b96142fbfc15d390b933854c
Omer <omerfarukkarakus01@gmail.com>
2020-07-14 09:14:02 +0100

    Python/Python Basics/Data Types and Useful Operations/Assignment-1 (Weekly Profit)

---
+++ Assignment-1 (Weekly Profit).py
@@ -0,0 +1,10 @@
+# Problem : If you had deposited a coin on the cryptocurrency exchange 
+# that brought 7% fixed profit daily for a week, 
+# how much would your $ 1000 reach at the end of the 7th day?
+
+deposit = 1000
+
+profit = deposit * 7 / 100 * 7
+total = deposit + profit
+
+print(total)
