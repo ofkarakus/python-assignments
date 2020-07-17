a9b70469b53d1cfa77f08ce8c54428a3914b4457
Omer <omerfarukkarakus01@gmail.com>
2020-07-17 18:08:44 +0100

    Python Basics\Control Flow Statements\Asg_1

---
+++ Assignment - 1 (if-Statements).py
@@ -0,0 +1,15 @@
+# Task : Let's say you left a message in the past that prints a password you need. To see the password you 
+# wrote, you need to enter your name and the program should recognize you.
+# Write a program that 
+
+# Takes the first name from the user and compares it to yours,
+# Then if the name the user entered is the same as yours, print out such as : "Hello, Joseph! The password 
+# is : W@12",
+# If the name the user entered is not the same as yours, print out such as : "Hello, Amina! See you later."
+
+
+first_name = input('Your first name, please: ').title().strip()
+if first_name == 'Joseph':
+    print('Hello, Joseph! The password is : W@12')
+else:
+    print(f'Hello, {first_name}! See you later.')
