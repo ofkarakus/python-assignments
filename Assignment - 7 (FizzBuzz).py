d71c91bc4c933a6c973a965a21e115426c8aa1aa
Omer <omerfarukkarakus01@gmail.com>
2020-07-18 08:52:49 +0100

    code is ready

--- Assignment - 7 (FizzBuzz).py
+++ Assignment - 7 (FizzBuzz).py
@@ -9,3 +9,11 @@
 # print the rest of the numbers unchanged.
 # Output each value on a separate line.
 
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else: print(i)
