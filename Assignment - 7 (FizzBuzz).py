7dd7e95e0ce4b2e739f2d04456ef214c6e7c5ecf
Omer <omerfarukkarakus01@gmail.com>
2020-07-18 08:47:25 +0100

    task explanation

---
+++ Assignment - 7 (FizzBuzz).py
@@ -0,0 +1,11 @@
+# Task : Print the FizzBuzz numbers.
+
+# FizzBuzz is a famous code challenge used in interviews to test basic programming 
+# skills. It's time to write your own implementation.
+# Print numbers from 1 to 100 inclusively following these instructions:
+# if a number is multiple of 3, print "Fizz" instead of this number,
+# if a number is multiple of 5, print "Buzz" instead of this number,
+# for numbers that are multiples of both 3 and 5, print "FizzBuzz",
+# print the rest of the numbers unchanged.
+# Output each value on a separate line.
+
