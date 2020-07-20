28cf418bc571f9feb2e26b0cc9adf370c2270e81
Omer <omerfarukkarakus01@gmail.com>
2020-07-20 15:41:21 +0100

    answer

--- question_answer_6.py
+++ question_answer_6.py
@@ -1,3 +1,17 @@
 # QUESTION:
-# Write a Python code that counts how many vowels and constants a string has that a 
+# Write a Python code that counts how many vowels and consonants a string has that a 
 # user entered.
+
+vowels = 'aeiou'
+consonants = 'bcdfghjklmnpqrstvwxyz'
+vows_cons = input('Enter a word, sentence, etc. : ')
+vows_counter = 0
+cons_counter = 0
+
+for i in vows_cons:
+    if i in vowels:
+        vows_counter += 1
+    elif i in consonants:
+        cons_counter += 1
+
+print(f'There are/is {vows_counter} vowel(s) and {cons_counter} consonant(s) in the input.')
