cea4721e8c40bec6c865c20a13f0ebd49e7b550b
Omer <omerfarukkarakus01@gmail.com>
2020-07-20 06:06:08 +0100

    comment added and code shortened

--- Assignment - 6 (Prime Numbers).py
+++ Assignment - 6 (Prime Numbers).py
@@ -1,11 +1,15 @@
 num = int(input('Enter a number : '))
 prime_nums =  []
 
-for i in range(1, num+1):
-    if i > 1:
-        for j in range(2, i):
-            if i % j == 0:
-                break
-        else:
-            prime_nums.append(i)
+for i in range(2, num+1):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        prime_nums.append(i)
 print(prime_nums)
+
+
+# --for içerisindeki tüm iterasyonlar bitince (exhaust = tükenince) else içerisindeki ifadeyi icra eder.
+# --yani for içerisnde "break" ile iterasyon kesintiye uğramazsa eğer, eninde sonunda iterasyon bitecek ve else yapısı mutlaka icra edilecektir.
+# --tabii, gene belirmek isterim ki ,for içerisnde break le iterasyon kesintiye uğratılmamışsa..
