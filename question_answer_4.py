fbee754b1c42c785e9f286eade283ff2abdc452f
Omer <omerfarukkarakus01@gmail.com>
2020-07-20 14:33:25 +0100

    answer

--- question_answer_4.py
+++ question_answer_4.py
@@ -3,3 +3,13 @@
 # elements of list = [999, 333, 2, 8982, 12, 45, 77, 99, 11]
 # Expected output:
 # [2, 11, 12, 45, 77, 99, 333, 999, 8982]
+
+ls = [999, 333, 2, 8982, 12, 45, 77, 99, 11]
+
+for i in range(len(ls)):
+    for j in range(i+1, len(ls)):
+        if ls[i] > ls[j]:
+            temp = ls[i]
+            ls[i] = ls[j]
+            ls[j] = temp
+print(ls)
