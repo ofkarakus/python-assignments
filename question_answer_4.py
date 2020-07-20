e78f4531cdd6aec85c4d8ba92d4f22e0bdab99b3
Omer <omerfarukkarakus01@gmail.com>
2020-07-20 14:27:00 +0100

    qa_4

---
+++ question_answer_4.py
@@ -0,0 +1,5 @@
+# QUESTION:
+# Write a Python code to sort the list at below without using .sort() method of list.
+# elements of list = [999, 333, 2, 8982, 12, 45, 77, 99, 11]
+# Expected output:
+# [2, 11, 12, 45, 77, 99, 333, 999, 8982]
