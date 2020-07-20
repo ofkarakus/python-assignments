e84ad5a17ccfdc943cc47aff540279e23325f22f
Omer <omerfarukkarakus01@gmail.com>
2020-07-20 16:43:13 +0100

    qa_7

---
+++ question_answer_7.py
@@ -0,0 +1,8 @@
+# Define a function with 'arbitrary numbers of arguments' method named as ‘spec_opr
+# (*numbers)’ . This function sums all of the arguments after a process. This process 
+# is even numbers should be divided by 2 and odd numbers should be multiplied with 3 
+# first and then added to the total sum.
+# Examples:
+# spec_opr(3, 2, 6) should return 13,
+# spec_opr(58, 53, 10, -10, -40, -28, 8, 34, 70, -16, 33) should return 301,
+# spec_opr(93, 12, 5, 84, -22) should return 331
