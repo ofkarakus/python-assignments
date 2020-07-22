b4a1a694d06d2fb5ebe5b45a0f0bcfa33168ecff
Omer <omerfarukkarakus01@gmail.com>
2020-07-22 11:50:16 +0100

    qa_9

---
+++ question_answer_9.py
@@ -0,0 +1,8 @@
+# Write a Python code that finds the students who have maximum and minimum average 
+# at a given dictionary below.
+# {'Student-1': {'Lesson-1': 57, 'Lesson-2': 46, 'Lesson-3': 58, 'Lesson-4': 81, 
+# 'Lesson-5': 85}, 'Student-2': {'Lesson-1': 85, 'Lesson-2': 56, 'Lesson-3': 51, 
+# 'Lesson-4': 69, 'Lesson-5': 67}, 'Student-3': {'Lesson-1': 68, 'Lesson-2': 76, 
+# 'Lesson-3': 87, 'Lesson-4': 57, 'Lesson-5': 56}, 'Student-4': {'Lesson-1': 78, 
+# 'Lesson-2': 93, 'Lesson-3': 88, 'Lesson-4': 38, 'Lesson-5': 54}, 'Student-5': 
+# {'Lesson-1': 50, 'Lesson-2': 46, 'Lesson-3': 78, 'Lesson-4': 81, 'Lesson-5': 75}}
