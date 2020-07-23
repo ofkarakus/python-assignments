1a0cc622d55794a76fdf7eb674abcdc0a58ac492
Omer <omerfarukkarakus01@gmail.com>
2020-07-23 12:23:34 +0100

    second solution added

--- Assignment - 6 (Prime Numbers).py
+++ Assignment - 6 (Prime Numbers).py
@@ -3,7 +3,7 @@
 # You can use a nested for loop.
 # Collect all these numbers into a list
 
-num = int(input('Enter a number : '))
+num = int(input('Enter the limit number : '))
 prime_nums = []
 
 for i in range(2, num+1):
@@ -15,6 +15,24 @@ for i in range(2, num+1):
 print(prime_nums)
 
 
-# --for içerisindeki tüm iterasyonlar bitince (exhaust = tükenince) else içerisindeki ifadeyi icra eder.
-# --yani for içerisnde "break" ile iterasyon kesintiye uğramazsa eğer, eninde sonunda iterasyon bitecek ve else yapısı mutlaka icra edilecektir.
-# --tabii, gene belirmek isterim ki ,for içerisnde break le iterasyon kesintiye uğratılmamışsa..
+# --for içerisindeki tüm iterasyonlar bitince (exhaust = tükenince) else 
+# içerisindeki ifadeyi icra eder.
+# --yani for içerisnde "break" ile iterasyon kesintiye uğramazsa eğer, eninde 
+# sonunda iterasyon bitecek ve else yapısı mutlaka icra edilecektir.
+# --tabii, gene belirmek isterim ki ,for içerisnde break le iterasyon kesintiye 
+# uğratılmamışsa..
+
+# ================================================================================
+
+# second solution
+
+n = int(input("Enter the limit number: "))
+prime = []
+for num in range(2, n+1):
+    status = True
+    for i in range(2, num):
+        if num % i ==0:
+            status = False
+    if status:
+        prime.append(num)
+print(prime)
