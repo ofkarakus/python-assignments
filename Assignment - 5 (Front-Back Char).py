2b6439f5af861c2e154279c355fad02f3129c946
Omer <omerfarukkarakus01@gmail.com>
2020-07-29 21:08:17 +0100

    answer added

--- Assignment - 5 (Front-Back Char).py
+++ Assignment - 5 (Front-Back Char).py
@@ -1,2 +1,7 @@
 # Given a string, return a new string where the first and last chars have been exchanged.
 
+def front_back(word):
+    if len(word) > 1:
+        return (word[-1] + word[1:-1] + word[0])
+    else:
+        return word
