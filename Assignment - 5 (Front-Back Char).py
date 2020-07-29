a63ea28e8eff548239c8919c8be7e4ea28e1c228
Omer <omerfarukkarakus01@gmail.com>
2020-07-29 21:09:42 +0100

    tests added

--- Assignment - 5 (Front-Back Char).py
+++ Assignment - 5 (Front-Back Char).py
@@ -5,3 +5,9 @@ def front_back(word):
         return (word[-1] + word[1:-1] + word[0])
     else:
         return word
+
+# Tests
+
+print(front_back('clarusway'))
+print(front_back('a'))
+print(front_back('ab'))
