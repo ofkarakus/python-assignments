68c1024400fcf63ae8ee7cb70c27a171fc88c188
Omer <omerfarukkarakus01@gmail.com>
2020-07-29 21:28:16 +0100

    answer

--- Assignment - 6 (Find Minimum).py
+++ Assignment - 6 (Find Minimum).py
@@ -3,3 +3,9 @@
 
 # Define a function named my_min to find the min of the inputted numbers.
 
+def my_min(*num):
+    temp = num[0]
+    for i in num:
+        if i < temp:
+            temp = i
+    return temp
